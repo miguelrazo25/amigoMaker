72ffe05145d8ece87c0d322f09aa3a2e6485f71b
miguel <thisismiguel1991@gmail.com>
2019-07-17 20:31:02 -0400

    folder structure

--- server.js
+++ server.js
@@ -1,8 +1,25 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
+
 
 const PORT = process.env.PORT || 8080;
 
+// create application/json parser
+var jsonParser = bodyParser.json()
+ 
+// create application/x-www-form-urlencoded parser
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+app.use(bodyParser.json({ type: 'application/*+json' }))
+ 
+// parse some custom thing into a Buffer
+app.use(bodyParser.raw({ type: 'application/vnd.custom-type' }))
+ 
+// parse an HTML body into a string
+app.use(bodyParser.text({ type: 'text/html' }))
+
 app.listen(PORT, function() {
-    console.log("App listening on PORT" + PORT);
+    console.log("App listening on PORT: " + PORT);
 })
+
